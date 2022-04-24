0ead28c14937eb4b8c4d2fabe6de01b0335c9b12
mogu4560 <mogu4560@gmail.com>
2022-04-24 16:58:00 +0530

    First-commit

---
+++ src/main/java/com/mycompany/myapp/repository/EmployeeRepository.java
@@ -0,0 +1,21 @@
+package com.mycompany.myapp.repository;
+
+import com.mycompany.myapp.domain.Employee;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.*;
+import org.springframework.stereotype.Repository;
+
+
+import java.util.Optional;
+
+/**
+ * Spring Data SQL repository for the Employee entity.
+ */
+@SuppressWarnings("unused")
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    Optional<Employee> findByEmployeeId(String employeeId);
+    @Query("select e from Employee e order by e.salary DESC, e.name")
+    Page<Employee> findByOrderBySalaryDescNameAsc(Pageable pageable);
+}
